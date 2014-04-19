758c727659065549ba477f019dbb8c0052759fd3
Diogo Lucas <diogo.lucas@gmail.com>
2014-04-19 17:27:51 -0300

    Cleaned up using directives, added a LookupHelper class

---
+++ RitterToDo/Core/LookupHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Moo;
+using RitterToDo.Models;
+using RitterToDo.Repos;
+
+namespace RitterToDo.Core
+{
+    public class LookupHelper<TEntity, TModel> where TEntity : class, IOwnedEntity
+    {
+        public IRepository<TEntity> Repo { get; private set; }
+        public IMappingRepository MappingRepo { get; private set; }
+
+        public LookupHelper(IRepository<TEntity> repo, IMappingRepository mappingRepo)
+        {
+            Repo = repo;
+            MappingRepo = mappingRepo;
+        }
+
+        public IEnumerable<TModel> GetAll()
+        {
+            var mapper = MappingRepo.ResolveMapper<TEntity, TModel>();
+            return mapper.MapMultiple(Repo.GetAll());
+        }
+    }
+}
